3ab0eec146383830195e08cc6f60f294b244387e
victor3232 <44395332+victor3232@users.noreply.github.com>
2025-07-07 13:43:59 +0700

    Update route.ts

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,6 +1,6 @@
 
 import { NextRequest } from 'next/server';
-import { updateSubdomainIP } from '@/lib/cloudflare';
+import { updateSubdomainIP } from '../../../../lib/cloudflare';
 
 export async function POST(req: NextRequest) {
   const { subdomain, ip, domain } = await req.json();
