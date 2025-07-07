cad04f0940f666d46e0d732fecd7f84102540887
victor3232 <44395332+victor3232@users.noreply.github.com>
2025-07-07 13:41:29 +0700

    Update route.ts

--- app/api/delete/route.ts
+++ app/api/delete/route.ts
@@ -1,6 +1,6 @@
 
 import { NextRequest } from 'next/server';
-import { deleteSubdomain } from '@/lib/cloudflare';
+import { deleteSubdomain } from '../../../../lib/cloudflare';
 
 export async function POST(req: NextRequest) {
   const { subdomain, domain } = await req.json();
